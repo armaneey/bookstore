d989a2fbaaa0dd8eb78da06e60b7f874e12638a0
armaneey <imanshuaibu1@gmail.com>
2026-05-11 13:39:32 -0700

    update: change base URL

--- frontend-v2/src/api/api.ts
+++ frontend-v2/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_BASE_URL = "https://bookstore-sooty-five.vercel.app";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
