e46c796c207ce336fd40ea77b44aac0851bdad70
armaneey <imanshuaibu1@gmail.com>
2026-05-12 08:07:12 -0700

    feat: Add routes to all pages

--- frontend-v2/src/lib/routes.ts
+++ frontend-v2/src/lib/routes.ts
@@ -1,3 +1,17 @@
-const home = () => "/"
+const home = () => '/';
+const createBooks = () => '/books/create';
+const detailsBooks = (id: string | number = ":id") => `/books/details/${id}`;
+const books = (id: string | number = ":id") => `/books/${id}`;
+const editBook = (id: string | number = ":id") => `/books/edit/${id}`;
+const deletesBooks = (id: string | number = ":id") => `/books/delete/${id}`;
+const searchBooks = () => '/books/search';
 
-export const routes = {home} 
+export const routes = {
+  home,
+  createBooks,
+  detailsBooks,
+  books,
+  editBook,
+  deletesBooks,
+  searchBooks,
+};
